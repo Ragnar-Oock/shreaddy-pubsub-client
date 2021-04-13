ed36933d59e11af7979958c38a12d0bebd0986f5
Ragnar-Oock <u.drgnk@outlook.com>
2021-04-13 15:07:04 +0200

    add event handler for message in commands topic and automatically send a command after connection to the topic

--- main.js
+++ main.js
@@ -1,12 +1,25 @@
 import PubSubClient from './pubsubClient.js';
 
-const url = 'wss://pubsub.warths.fr/';
+const subscribeUrl = 'wss://pubsub.warths.fr/';
+const publishUrl = 'https://pubsub.warths.fr/publish';
 
-const client = new PubSubClient(url);
+const client = new PubSubClient(subscribeUrl, publishUrl);
 
 client.addEventListener('socket:open', () => {
 	client.subscribe('commands')
-		.then(() => console.log('subscribed!'));
+		.then(() => {
+			console.log('subscribed!');
+			client.sendCommand('vip', ['a', 'b'], import.meta.env.VITE_AUTH_TOKEN);
+
+		});
 });
 
-client.addEventListener('client:message', msg => console.log(msg));
+client.addEventListener('client:message', event => {
+	if (event.topic === 'commands') {
+		const parameters = event.message.parameters.reduce((list, param) => list = `${ list }, ${ param }`);
+
+		console.log(`${ event.message.user.display_name } issued the command ${ event.message.command } with the parameters ${ parameters }`);
+	}
+});
+
+client.addEventListener('socket:message', msg => console.log(msg));
